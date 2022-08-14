14dee095cb8e4934820fc7706f7bf6a9eda6b0aa
mominul <mominsjob@gmail.com>
2022-08-14 10:31:25 +0600

    flash screen

---
+++ app/src/main/java/com/mominulcse7/githubclone/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.mominulcse7.githubclone
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        finish()
+    }
+}
